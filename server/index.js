7a5d3535cfea7d0de9d00f30940dbabafd9f513a
Nick Simpson <sylviasimpson@Sylvias-MacBook-Air.local>
2017-08-09 22:52:23 -0500

    added count method

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 
+function Planet(req, res){
+  res.send('Save the planet man!!!!!!!!!');
+}
+var num = 0;
+
+function count(req, res){
+  num = num+1;
+  res.send(`The count is: ${num}`);
+}
+
 var app = express();
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.get('/captinPlanet', function(req, res){
-  res.send('Save the planet man!');
-})
+app.get('/captinPlanet', Planet)
+
+app.get("/count", count)
+
 
 app.listen('8080', function(){ console.log('listening on port 8080'); });
