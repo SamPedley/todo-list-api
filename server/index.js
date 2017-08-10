38dfb42dd8499ce3124dc8d92cd4461705642564
sam pedley <sjpeds@gmail.com>
2017-08-09 22:11:17 -0500

    added a hello world route

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log('Helloo worldzzzzzzzz');
+const express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen('8080', function(){ console.log('listening on port 8080'); });
+
